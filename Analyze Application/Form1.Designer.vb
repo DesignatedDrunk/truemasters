eda9259539f960747f527fdc3f7b5c5b8817000a
sander@booxs.nl <Jbooxs432>
2016-04-30 20:09:25 +0200

    Initial Release

---
+++ Analyze Application/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tbAPIKey = New System.Windows.Forms.TextBox()
+        Me.pbPRogress = New System.Windows.Forms.ProgressBar()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblProgress = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tbDir = New System.Windows.Forms.TextBox()
+        Me.tbLog = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnDownloadPlayers = New System.Windows.Forms.Button()
+        Me.btnDownloadChampions = New System.Windows.Forms.Button()
+        Me.bwDownloadChampions = New System.ComponentModel.BackgroundWorker()
+        Me.bwDownloadPlayers = New System.ComponentModel.BackgroundWorker()
+        Me.btnAnalyzePlayerData = New System.Windows.Forms.Button()
+        Me.bwAnalyzePlayers = New System.ComponentModel.BackgroundWorker()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "API Key"
+        '
+        'tbAPIKey
+        '
+        Me.tbAPIKey.Location = New System.Drawing.Point(58, 20)
+        Me.tbAPIKey.Name = "tbAPIKey"
+        Me.tbAPIKey.Size = New System.Drawing.Size(150, 20)
+        Me.tbAPIKey.TabIndex = 1
+        Me.tbAPIKey.Text = ""
+        '
+        'pbPRogress
+        '
+        Me.pbPRogress.Location = New System.Drawing.Point(10, 72)
+        Me.pbPRogress.Name = "pbPRogress"
+        Me.pbPRogress.Size = New System.Drawing.Size(198, 23)
+        Me.pbPRogress.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblProgress)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.tbDir)
+        Me.GroupBox1.Controls.Add(Me.tbLog)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.tbAPIKey)
+        Me.GroupBox1.Controls.Add(Me.pbPRogress)
+        Me.GroupBox1.Location = New System.Drawing.Point(718, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(218, 369)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Progress"
+        '
+        'lblProgress
+        '
+        Me.lblProgress.Location = New System.Drawing.Point(10, 98)
+        Me.lblProgress.Name = "lblProgress"
+        Me.lblProgress.Size = New System.Drawing.Size(198, 22)
+        Me.lblProgress.TabIndex = 6
+        Me.lblProgress.Text = "..."
+        Me.lblProgress.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Save"
+        '
+        'tbDir
+        '
+        Me.tbDir.Location = New System.Drawing.Point(58, 46)
+        Me.tbDir.Name = "tbDir"
+        Me.tbDir.Size = New System.Drawing.Size(150, 20)
+        Me.tbDir.TabIndex = 4
+        Me.tbDir.Text = "D:\Installed x64\inetpub\wwwroot\RiotApi\RiotApi\Export"
+        '
+        'tbLog
+        '
+        Me.tbLog.Location = New System.Drawing.Point(10, 123)
+        Me.tbLog.Multiline = True
+        Me.tbLog.Name = "tbLog"
+        Me.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.tbLog.Size = New System.Drawing.Size(198, 230)
+        Me.tbLog.TabIndex = 3
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnAnalyzePlayerData)
+        Me.GroupBox2.Controls.Add(Me.btnDownloadPlayers)
+        Me.GroupBox2.Controls.Add(Me.btnDownloadChampions)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 299)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(700, 82)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "General"
+        '
+        'btnDownloadPlayers
+        '
+        Me.btnDownloadPlayers.Location = New System.Drawing.Point(7, 53)
+        Me.btnDownloadPlayers.Name = "btnDownloadPlayers"
+        Me.btnDownloadPlayers.Size = New System.Drawing.Size(190, 23)
+        Me.btnDownloadPlayers.TabIndex = 1
+        Me.btnDownloadPlayers.Text = "Download PlayerData"
+        Me.btnDownloadPlayers.UseVisualStyleBackColor = True
+        '
+        'btnDownloadChampions
+        '
+        Me.btnDownloadChampions.Location = New System.Drawing.Point(7, 20)
+        Me.btnDownloadChampions.Name = "btnDownloadChampions"
+        Me.btnDownloadChampions.Size = New System.Drawing.Size(190, 23)
+        Me.btnDownloadChampions.TabIndex = 0
+        Me.btnDownloadChampions.Text = "Download ChampionData"
+        Me.btnDownloadChampions.UseVisualStyleBackColor = True
+        '
+        'bwDownloadChampions
+        '
+        Me.bwDownloadChampions.WorkerReportsProgress = True
+        '
+        'bwDownloadPlayers
+        '
+        Me.bwDownloadPlayers.WorkerReportsProgress = True
+        '
+        'btnAnalyzePlayerData
+        '
+        Me.btnAnalyzePlayerData.Location = New System.Drawing.Point(203, 20)
+        Me.btnAnalyzePlayerData.Name = "btnAnalyzePlayerData"
+        Me.btnAnalyzePlayerData.Size = New System.Drawing.Size(190, 23)
+        Me.btnAnalyzePlayerData.TabIndex = 2
+        Me.btnAnalyzePlayerData.Text = "Analyze PlayerData"
+        Me.btnAnalyzePlayerData.UseVisualStyleBackColor = True
+        '
+        'bwAnalyzePlayers
+        '
+        Me.bwAnalyzePlayers.WorkerReportsProgress = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(948, 393)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents tbAPIKey As System.Windows.Forms.TextBox
+    Friend WithEvents pbPRogress As System.Windows.Forms.ProgressBar
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents tbLog As System.Windows.Forms.TextBox
+    Friend WithEvents btnDownloadChampions As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents tbDir As System.Windows.Forms.TextBox
+    Friend WithEvents bwDownloadChampions As System.ComponentModel.BackgroundWorker
+    Friend WithEvents lblProgress As System.Windows.Forms.Label
+    Friend WithEvents btnDownloadPlayers As System.Windows.Forms.Button
+    Friend WithEvents bwDownloadPlayers As System.ComponentModel.BackgroundWorker
+    Friend WithEvents btnAnalyzePlayerData As System.Windows.Forms.Button
+    Friend WithEvents bwAnalyzePlayers As System.ComponentModel.BackgroundWorker
+
+End Class
---
+++ Analyze Application/Form1.vb
@@ -0,0 +1,272 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Web.Script.Serialization
+Imports System.Threading
+
+Public Class Form1
+    Public Shared Property Regions As String() = {"euw", "br", "eune", "jp", "kr", "lan", "las", "na", "oce", "ru", "tr"}
+    Public Shared Property MasteryRegions As String() = {"EUW1", "BR1", "EUN1", "JP1", "KR", "LA1", "LA2", "NA1", "OC1", "RU", "TR1"}
+
+    Public ReadOnly Property ApiKey As String
+        Get
+            Return tbAPIKey.Text
+        End Get
+    End Property
+    Public ReadOnly Property SaveDirectory As DirectoryInfo
+        Get
+            Return New DirectoryInfo(tbDir.Text)
+        End Get
+    End Property
+    Public ReadOnly Property ApiUrl(ByVal _region As Int32, ByVal _version As String, ByVal _function As String, Optional ByVal _args As String = "", Optional ByVal isglobal As Boolean = False, Optional ByVal ismastery As Boolean = False) As String
+        Get
+            If isglobal Then
+                Return "https://global.api.pvp.net/api/lol/static-data/" & Regions(_region) & "/" & _version & "/" & _function & "?api_key=" & ApiKey & _args
+            ElseIf ismastery Then
+                Return "https://" & Regions(_region) & ".api.pvp.net/championmastery/location/" & MasteryRegions(_region) & "/" & _function & "?api_key=" & ApiKey & _args
+            Else
+                Return "https://" & Regions(_region) & ".api.pvp.net/api/lol/" & Regions(_region) & "/" & _version & "/" & _function & "?api_key=" & ApiKey & _args
+            End If
+        End Get
+    End Property
+
+    Private Sub btnDownloadChampions_Click(sender As Object, e As EventArgs) Handles btnDownloadChampions.Click
+        pbPRogress.Value = 0
+        lblProgress.Text = "Starting ..."
+        bwDownloadChampions.RunWorkerAsync()
+    End Sub
+    Private Sub bwDownloadChampions_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles bwDownloadChampions.DoWork
+        Dim _error As Object = Nothing
+        Dim _result As String = MakeRequest(ApiUrl(0, "v1.2", "champion"), _error) 'Download all ChampionIDs
+
+        'Retrieve Static ChampionInfo
+        If _error Is Nothing And Not String.IsNullOrEmpty(_result) Then
+            Dim _fi As New FileInfo(Path.Combine(SaveDirectory.FullName, "data_champions.js"))
+            If _fi.Exists Then _fi.Delete()
+
+            Dim js As New JavaScriptSerializer()
+            js.MaxJsonLength = Int32.MaxValue
+
+            Dim championsDictionary As Dictionary(Of String, Object) = js.DeserializeObject(_result)
+            Dim saveDictionary As New Dictionary(Of String, Object)()
+
+            Dim lst As Object() = championsDictionary("champions")
+            Dim i As Int32 = 0
+            For Each d As Dictionary(Of String, Object) In lst
+                Dim championID As String = d("id")
+                _result = MakeRequest(ApiUrl(0, "v1.2", "champion/" & championID, "&champData=all", True), _error) 'Download Details Champion
+                If _error Is Nothing And Not String.IsNullOrEmpty(_result) Then
+                    saveDictionary.Add(championID, js.DeserializeObject(_result))
+                ElseIf Not _error Is Nothing Then
+                    bwDownloadChampions.ReportProgress(100, New String() {"Error Downloading Champion [" & CType(_error, WebException).ToString() & "] ..."})
+                End If
+
+                bwDownloadChampions.ReportProgress(i / lst.Length * 100, (i + 1) & " / " & lst.Length & " champions")
+                i += 1
+            Next
+            File.WriteAllText(_fi.FullName, "var data_champions = " & js.Serialize(saveDictionary))
+        ElseIf Not _error Is Nothing Then
+            bwDownloadChampions.ReportProgress(100, New String() {"Error Downloading Champions [" & CType(_error, WebException).ToString() & "] ..."})
+        End If
+    End Sub
+
+    Private Sub btnDownloadPlayers_Click(sender As Object, e As EventArgs) Handles btnDownloadPlayers.Click
+        pbPRogress.Value = 0
+        lblProgress.Text = "Starting ..."
+        bwDownloadPlayers.RunWorkerAsync()
+    End Sub
+    Private Sub bwDownloadPlayers_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles bwDownloadPlayers.DoWork
+        Dim _error As Object = Nothing
+        Dim _result As String = Nothing
+        Dim result As New Dictionary(Of String, Object)()
+
+        Dim js As New JavaScriptSerializer()
+        js.MaxJsonLength = Int32.MaxValue
+
+        'Get Players From these tiers
+        Dim _ranks As String() = {"master", "challenger"}
+        Dim cnter As Int32 = 0
+        For Each _rank As String In _ranks
+            Dim rankDictionary As New List(Of Object)()
+
+            'Get From All Regions
+            For i As Int32 = 0 To Regions.Length - 1
+                _result = MakeRequest(ApiUrl(i, "v2.5", "league/" & _rank, "&type=RANKED_SOLO_5x5"), _error) 'Download Players
+                bwDownloadPlayers.ReportProgress((i + cnter) / (2 * Regions.Length) * 100, (i + cnter + 1) & " / " & (2 * Regions.Length) & " tiers / regions")
+
+                If _error Is Nothing And Not String.IsNullOrEmpty(_result) Then
+                    Dim playersDictionary As Dictionary(Of String, Object) = js.DeserializeObject(_result)
+                    Dim lst As Object() = playersDictionary("entries")
+                    Dim lsti As Int32 = 0
+                    For Each player As Dictionary(Of String, Object) In lst
+                        bwDownloadPlayers.ReportProgress((i + cnter) / (2 * Regions.Length) * 100, (i + cnter + 1) & " / " & (2 * Regions.Length) & " tiers / regions (" & (lsti + 1) & "/" & lst.Length & ")")
+                        player.Add("region", Regions(i))
+
+                        Dim playerID As String = player("playerOrTeamId")
+
+                        _result = Nothing
+                        _error = Nothing
+                        While Not _error Is Nothing Or String.IsNullOrEmpty(_result)
+                            _result = MakeRequest(ApiUrl(i, "", "player/" & playerID & "/topchampions", "", False, True), _error) 'Download Player MasteryData
+
+                            If _error Is Nothing And Not String.IsNullOrEmpty(_result) Then
+                                Dim masteryArray As Object() = js.DeserializeObject(_result)
+                                player.Add("masterydata", masteryArray)
+                            ElseIf Not _error Is Nothing Then
+                                bwDownloadPlayers.ReportProgress((i + cnter) / (2 * Regions.Length) * 100, New String() {"Error Downloading Mastery [" & CType(_error, WebException).ToString() & "] ..."})
+                                bwDownloadPlayers.ReportProgress((i + cnter) / (2 * Regions.Length) * 100, New String() {"Error Downloading Mastery [" & CType(_error, WebException).ToString() & "] ..."})
+                                bwDownloadPlayers.ReportProgress((i + cnter) / (2 * Regions.Length) * 100, New String() {"Error Downloading Mastery [" & CType(_error, WebException).ToString() & "] ..."})
+                            End If
+
+                            If Not _error Is Nothing Or String.IsNullOrEmpty(_result) Then Thread.Sleep(15000)
+                        End While
+
+                        rankDictionary.Add(player)
+                        lsti += 1
+                    Next
+                ElseIf Not _error Is Nothing Then
+                    bwDownloadPlayers.ReportProgress(100, New String() {"Error Downloading LEague [" & CType(_error, WebException).ToString() & "] ..."})
+                End If
+            Next
+
+            result.Add(_rank, rankDictionary.ToArray())
+            cnter += Regions.Length
+        Next
+
+        Dim _fi As New FileInfo(Path.Combine(SaveDirectory.FullName, "data_players.js"))
+        If _fi.Exists Then _fi.Delete()
+        File.WriteAllText(_fi.FullName, "var data_players = " & js.Serialize(result))
+    End Sub
+
+
+    Private Sub btnAnalyzePlayerData_Click(sender As Object, e As EventArgs) Handles btnAnalyzePlayerData.Click
+        pbPRogress.Value = 0
+        lblProgress.Text = "Starting ..."
+        bwAnalyzePlayers.RunWorkerAsync()
+    End Sub
+    Private Sub bwAnalyzePlayers_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles bwAnalyzePlayers.DoWork
+        Dim js As New JavaScriptSerializer()
+        js.MaxJsonLength = Int32.MaxValue
+
+        Dim fiChampions As New FileInfo(Path.Combine(SaveDirectory.FullName, "data_champions.js"))
+        Dim objChampions As Dictionary(Of String, Object) = js.DeserializeObject(File.ReadAllText(fiChampions.FullName).Substring(21)) 'Remove Javascript prefix
+
+        Dim fiPlayers As New FileInfo(Path.Combine(SaveDirectory.FullName, "data_players.js"))
+        Dim objPlayers As Dictionary(Of String, Object) = js.DeserializeObject(File.ReadAllText(fiPlayers.FullName).Substring(19)) 'Remove Javascript prefix
+
+        'Setup Empty
+        Dim result As New Dictionary(Of String, Object)()
+        result.Add("global", New Dictionary(Of String, Object)()) 'Global stats
+        For Each _region As String In Regions
+            result.Add(_region, New Dictionary(Of String, Object)())
+        Next
+
+        For Each kvp As KeyValuePair(Of String, Object) In result
+            kvp.Value.Add("global", New Dictionary(Of String, Object)()) 'All Tiers Stats
+            For Each kvp2 As KeyValuePair(Of String, Object) In objPlayers 'Loop Tiers
+                kvp.Value.Add(kvp2.Key, New Dictionary(Of String, Object)())
+            Next
+            For Each kvp2 As KeyValuePair(Of String, Object) In kvp.Value
+                For Each kvp3 As KeyValuePair(Of String, Object) In objChampions 'Loop Champions
+                    Dim d As New Dictionary(Of String, Object)()
+                    d.Add("amount_lvl1", 0) 'To store all #1 playercounts
+                    d.Add("amount_lvl2", 0) 'To store all #2 playercounts
+                    d.Add("amount_lvl3", 0) 'To store all #3 playercounts
+                    d.Add("amount_lvl", 0) 'To store all playercounts (#1 = 3pnt, #2 = 2pnt, #3 = 1pnt)
+                    d.Add("amount_points", 0) 'To store total masterypoints
+                    d.Add("champions", New Dictionary(Of String, Object)()) 'To store all connected champions
+                    For Each kvp4 As KeyValuePair(Of String, Object) In objChampions 'Loop Champions
+                        d("champions").Add(kvp4.Key, 0)
+                    Next
+
+                    kvp2.Value.Add(kvp3.Key, d)
+                Next
+            Next
+        Next
+
+        'Fill data
+        For Each kvpTier As KeyValuePair(Of String, Object) In objPlayers
+            Dim _tier As String = kvpTier.Key
+            For Each _player As Dictionary(Of String, Object) In kvpTier.Value
+                Dim _region As String = _player("region")
+                If _player.ContainsKey("masterydata") Then
+                    Dim _masterydata As Object() = _player("masterydata")
+                    Dim _saveRegions As String() = {"global", _region}
+                    Dim _saveTiers As String() = {"global", _tier}
+
+                    For _spot As Int32 = 0 To _masterydata.Length - 1
+                        Dim _championdata As Dictionary(Of String, Object) = _masterydata(_spot)
+                        
+                        For Each sRegion As String In _saveRegions
+                            For Each sTier As String In _saveTiers
+                                result(sRegion)(sTier)(_championdata("championId"))("amount_lvl" & (_spot + 1)) += 1
+                                result(sRegion)(sTier)(_championdata("championId"))("amount_lvl") += (3 - _spot) ' (#0 = 3pnt, #1 = 2pnt, #2 = 1pnt)
+                                result(sRegion)(sTier)(_championdata("championId"))("amount_points") += _championdata("championPoints")
+
+                                'Save Connected Champions
+                                For _secundaryspot As Int32 = 0 To _masterydata.Length - 1
+                                    If Not _spot = _secundaryspot Then
+                                        result(sRegion)(sTier)(_championdata("championId"))("champions")(_masterydata(_secundaryspot)("championId")) += 1
+                                    End If
+                                Next
+                            Next
+                        Next
+                    Next
+                End If
+            Next
+        Next
+
+        Dim _fi As New FileInfo(Path.Combine(SaveDirectory.FullName, "data_regions.js"))
+        If _fi.Exists Then _fi.Delete()
+        File.WriteAllText(_fi.FullName, "var data_regions = " & js.Serialize(result))
+    End Sub
+
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles bwDownloadChampions.ProgressChanged, bwDownloadPlayers.ProgressChanged, bwAnalyzePlayers.ProgressChanged
+        pbPRogress.Value = e.ProgressPercentage
+
+        If TypeOf e.UserState Is String Then
+            lblProgress.Text = CType(e.UserState, String)
+        ElseIf TypeOf e.UserState Is String() Then
+            tbLog.Text &= String.Join(vbCrLf, CType(e.UserState, String()))
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles bwDownloadChampions.RunWorkerCompleted, bwDownloadPlayers.RunWorkerCompleted, bwAnalyzePlayers.RunWorkerCompleted
+        pbPRogress.Value = 100
+        lblProgress.Text = "... Completed"
+    End Sub
+
+    Public Function MakeRequest(ByVal url As String, ByRef _error As Object) As String
+        _error = Nothing
+
+        'Create Request
+        Dim request As WebRequest = WebRequest.Create(url)
+        'request.ContentType = "application/x-www-form-urlencoded"
+        request.Method = "GET"
+
+        'Write Body
+        Dim requestStream As Stream = Nothing
+
+        'Get Response
+        Dim response As WebResponse = Nothing
+        Dim responseReader As StreamReader = Nothing
+        Try
+            response = request.GetResponse()
+            requestStream = response.GetResponseStream()
+            responseReader = New StreamReader(requestStream)
+            Dim responseString As String = responseReader.ReadToEnd()
+
+            Return responseString
+        Catch ex As WebException
+            _error = ex
+        Finally
+            'Close Readers / Writers
+            If Not responseReader Is Nothing Then responseReader.Close()
+            If Not requestStream Is Nothing Then requestStream.Close()
+            If Not response Is Nothing Then response.Close()
+        End Try
+
+        'Handle Result
+        Return Nothing
+    End Function
+End Class
